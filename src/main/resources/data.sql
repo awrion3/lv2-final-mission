206e204fc264b3af80d1428d9335e96c49042f93
RIWON <121172065+awrion3@users.noreply.github.com>
2025-06-24 13:32:33 +0900

    refactor: update sql, setup files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,11 @@ INSERT INTO reservation_time(start_at)
 VALUES ('12:00');
 
 INSERT INTO member(name, email, password, role)
-VALUES ('admin', 'wooteco@gmail.com', '1234', 'admin');
+VALUES ('admin', 'wooteco@gmail.com', '1234', 'ADMIN');
 INSERT INTO member(name, email, password, role)
-VALUES ('riwon', 'riwon@gmail.com', '5678', 'user');
+VALUES ('riwon', 'riwon@gmail.com', '5678', 'MEMBER');
 
-INSERT INTO reservation(name, date, time_id, member_id)
-VALUES ('admin', '2025-05-25', 1, 1);
-INSERT INTO reservation(name, date, time_id, member_id)
-VALUES ('riwon', '2025-05-25', 2, 2);
+INSERT INTO reservation(date, time_id, member_id)
+VALUES ('2025-06-25', 1, 1);
+INSERT INTO reservation(date, time_id, member_id)
+VALUES ('2025-06-25', 2, 2);
